2bbb15a2750f9c2915b66328eee77053a97df2e8
ebarroso2214 <ebarroso2214@gmail.com>
2023-06-26 20:28:40 -0400

    enabled cookies

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -4,6 +4,20 @@ const bcrypt = require('bcrypt');
 
 const {User} = db;
 
+router.get('/profile', async (req,res) => {
+    // console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where:{
+                userId: req.session.userId,
+            }
+        })
+        res.json(user)
+    }catch(err){
+        res.json(null)
+    }
+})
+
 router.post('/', async (req, res) => {
     const user = await User.findOne({
         where: {email: req.body.email}
@@ -14,6 +28,7 @@ router.post('/', async (req, res) => {
             message: 'Could not find a user with the provided username and password'
         })
     } else {
+        req.session.userId = user.userId
         res.json({user})
     }
 
