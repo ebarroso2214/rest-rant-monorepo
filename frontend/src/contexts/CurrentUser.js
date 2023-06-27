a127184f98924802b0938d08c16ab7a3c205c13b
ebarroso2214 <ebarroso2214@gmail.com>
2023-06-26 21:04:14 -0400

    implemented use of JWT for userId to keep users logged in after refreshing page

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CurrentUser = createContext()
@@ -6,7 +6,21 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
-    window.setCurrentUser = setCurrentUser
+    useEffect(()=> {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                headers: {
+                    'Authorization' : `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            let user = await response.json();
+            // console.log(user)
+            setCurrentUser(user);
+        }
+
+        getLoggedInUser();
+    }, []);
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -29,12 +29,13 @@ function LoginForm() {
 
         if (response.status === 200){
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push('/')
         } else{
             setErrorMessage(data.message)
         }
 
-        console.log(data)
+        // console.log(data)
     }
 
     return (
